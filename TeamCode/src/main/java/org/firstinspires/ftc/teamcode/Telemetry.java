f1f4e121842f79c4a6ccec39082a6320b7253039
justclaner <justclaner@gmail.com>
2023-10-07 14:10:44 -0400

    driving

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Telemetry.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Telemetry.java
@@ -3,24 +3,31 @@ import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
-import com.qualcomm.robotcore.hardware.Servo;
-import com.qualcomm.robotcore.hardware.ServoController;
-import com.qualcomm.robotcore.util.ElapsedTime;
-
-import com.qualcomm.robotcore.hardware.DcMotor;
+//import com.qualcomm.robotcore.hardware.Servo;
+//import com.qualcomm.robotcore.hardware.ServoController;
+//import com.qualcomm.robotcore.util.ElapsedTime;
+//
+//import com.qualcomm.robotcore.hardware.DcMotor;
 @TeleOp(name = "TeleOp WIP")
 public class Telemetry extends OpMode {
-    public DcMotor frontLeft;
-    public DcMotor frontRight;
-    public DcMotor backLeft;
-    public DcMotor backRight;
+     DcMotor frontLeft;
+     DcMotor frontRight;
+     DcMotor backLeft;
+     DcMotor backRight;
 
     @Override
     public void init() {
-        frontRight.setDirection(DcMotorSimple.Direction.REVERSE);
-        backRight.setDirection(DcMotorSimple.Direction.REVERSE);
-    }
 
+
+        frontLeft = hardwareMap.get(DcMotor.class, "frontLeft");
+        frontRight = hardwareMap.get(DcMotor.class, "frontRight");
+        backLeft = hardwareMap.get(DcMotor.class, "backLeft");
+        backRight = hardwareMap.get(DcMotor.class, "backRight");
+
+        backLeft.setDirection(DcMotorSimple.Direction.REVERSE);
+
+    }
+@Override
     public void loop() {
         double xAxisMovement = gamepad1.left_stick_x;
         double yAxisMovement = gamepad1.left_stick_y;
@@ -28,15 +35,16 @@ public class Telemetry extends OpMode {
         double lAxisMovement = gamepad2.left_stick_y;
 
 
-        double frontLeftPower = yAxisMovement + xAxisMovement - rAxisMovement;
-        double frontRightPower = yAxisMovement - xAxisMovement + rAxisMovement;
-        double backLeftPower = yAxisMovement - xAxisMovement - rAxisMovement;
-        double backRightPower = yAxisMovement + xAxisMovement + rAxisMovement;
+        double frontLeftPower = yAxisMovement - xAxisMovement - rAxisMovement;
+        double frontRightPower = yAxisMovement + xAxisMovement + rAxisMovement;
+        double backLeftPower = yAxisMovement + xAxisMovement - rAxisMovement;
+        double backRightPower = yAxisMovement - xAxisMovement + rAxisMovement;
 
         frontLeft.setPower(frontLeftPower);
-        frontRight.setPower(frontLeftPower);
+        frontRight.setPower(frontRightPower);
         backLeft.setPower(backLeftPower);
         backRight.setPower(backRightPower);
+        //comment
     }
 
 }
